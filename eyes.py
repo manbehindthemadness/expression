fa2204fe59d95ddd5456fcc67b35ead6b27417bc
Kevin Eales <manbehindthemadness@gmail.com>
2022-09-05 09:07:21 -0400

    Improved realism.

--- eyes.py
+++ eyes.py
@@ -70,12 +70,8 @@ def display_eye_init(display, side):
 
 
 displays = Display(reset, command0, command1, cs0, cs1)
-display_L, eyeball_L, iris_L, exp_up_L, exp_down_L, blink_L= display_eye_init(displays.displays[0], 'left')
+display_L, eyeball_L, iris_L, exp_up_L, exp_down_L, blink_L = display_eye_init(displays.displays[0], 'left')
 display_R, eyeball_R, iris_R, exp_up_R, exp_down_R, blink_R = display_eye_init(displays.displays[1], 'right')
-
-thetaL, thetaR = 0, 0  # left & right eye rotational position
-dthetaL, dthetaR = 0.25, 0.25  # how fast left & right eyes spins
-r = 17  # size of eye spin
 
 
 def eye_position(x, y):
@@ -89,6 +85,21 @@ def eye_position(x, y):
     iris_R.y = y
 
     return int(iris_L.x), int(iris_R.y)
+
+
+def eye_roll(x_anchor, y_anchor, rad):
+    """
+    Adds some radial movements.
+    """
+    thetaL, thetaR = 0, 0  # left & right eye rotational position
+    dthetaL, dthetaR = 0.25, 0.25  # how fast left & right eyes spins
+
+    iris_L.x = iris_cx + int(rad * math.sin(thetaL))
+    iris_L.y = iris_cy + int(rad * math.cos(thetaL))
+    iris_R.x = iris_cx + int(rad * math.sin(thetaL))
+    iris_R.y = iris_cy + int(rad * math.cos(thetaL))
+    thetaL -= dthetaL  # update angles (negative for clockwise motion)
+    thetaR -= dthetaR
 
 
 def saccades(x, y, x_anchor, y_anchor):
@@ -108,33 +119,30 @@ def saccades(x, y, x_anchor, y_anchor):
 xx, yy = eye_position(48, 32)
 
 
-print("MEMORY ALLOCATED", gc.mem_alloc())
-print("MEMORY FREE", gc.mem_free())
+print("MEMORY ALLOCATED", gc.mem_alloc())  # noqa
+print("MEMORY FREE", gc.mem_free())  # noqa
 shut = False
+refresh = True
 while True:
     if shut:  # If eyes are shut, open them.
         blink_L.x = -200
         blink_R.x = -200
         shut = False
-
-    # iris_L.x = iris_cx + int(r * math.sin(thetaL))
-    # iris_L.y = iris_cy + int(r * math.cos(thetaL))
-    # iris_R.x = iris_cx + int(r * math.sin(thetaL))
-    # iris_R.y = iris_cy + int(r * math.cos(thetaL))
+        refresh = True
 
     if not random.randint(0, 2):
-        saccades(15, 15, xx, yy)
+        saccades(7, 7, xx, yy)
+        refresh = True
 
-    if not random.randint(0, 150):  # Blink randomly.
+    if not random.randint(0, 50):  # Blink randomly.
         blink_L.x = 0
         blink_R.x = 0
         shut = True
+        refresh = True
 
-    thetaL -= dthetaL  # update angles (negative for clockwise motion)
-    thetaR -= dthetaR
-
-    display_L.refresh(target_frames_per_second=20)
-    display_R.refresh(target_frames_per_second=20)
-    delay = random.randint(500, 1000) / 1000
-
+    if refresh:
+        display_L.refresh(target_frames_per_second=20)
+        display_R.refresh(target_frames_per_second=20)
+    delay = random.randint(250, 750) / 1000
+    refresh = False
     time.sleep(delay)
